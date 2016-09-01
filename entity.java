9de43b14457e5db47f1237a04dce9049a414691e
Josh Wiedemeier <joshuaw-GitHub@jandjw.com>
2016-09-01 09:34:33 -0500

    Create Entity.java

---
+++ entity.java
@@ -0,0 +1,77 @@
+package Game;
+
+public class Entity{
+  private int currentHealth,maxHealth,speed,posX,posY;
+  private String type;
+  private Animation[] animations;
+  
+  public Entity(){
+    currentHealth=maxHealth=100;
+    speed=1;
+    posX=posY=0;
+    type="NULL";
+    animations=new Animation[10];
+  }
+  public Entity(int currentHealth,int maxHealth, int speed, int posX, int posY, String type, Animation[] animations){
+    this.currentHealth=currentHealth;
+    this.maxHealth=maxHealth;
+    this.speed=speed;
+    this.posX=posX;
+    this.posY=posY;
+    this.type=type;
+    this.animations=animations;
+  }
+  //end constructors
+  
+  //begin get methods
+  public int getCurrentHealth(){
+    return currentHealth;
+  }
+  public int getmaxHealth(){
+    return maxHealth
+  }
+  public int getSpeed(){
+    return speed;
+  }
+  public int getPosX(){
+    return posX;
+  }
+  public int getPosY(){
+    return posY;
+  }
+  public String getType(){
+    return type;
+  }
+  public Animation[] getAnimations(){
+    return animations;
+  }
+  //end get methods
+  
+  //begin set methods
+  public void setCurrentHealth(int health){
+    currentHealth=health;
+  }
+  public void setMaxHealth(int health){
+    maxHealth=health;
+  }
+  public void setSpeed(int speed){
+    this.speed;
+  }
+  public void setPosX(int posX){
+    this.posX=posX;
+  }
+  public void setposY(int posY){
+    this.posY=posY
+  }
+  public void setType(String type){
+    this.type=type;
+  }
+  public void setAnimations(Animation[] animations){
+    this.animations=animations;
+  }
+  //end set methods
+
+  public void doAction(Action action){
+    action.execute(this);
+  }
+}
