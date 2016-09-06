d29e9eb6d44c93dea655e61ffc0cb49012d084d1
Josh Wiedemeier <joshuaw-GitHub@jandjw.com>
2016-09-06 08:58:20 -0500

    adds inventoryAnimation to item class

--- Item.java
+++ Item.java
@@ -5,6 +5,7 @@ public class Item {
 	
 	private String type;
 	private int dmg,value,armor,keyID,healthRestore,manaRestore;
+	private Animation InventoryAnimation;
 	private Action[] actions;
 	
 	public Item(){
@@ -18,6 +19,9 @@ public class Item {
 	
 	public void doAction(int actionID){
 		actions[actionID].execute(this);
+	}
+	public Animation getInventoryAnimation(){
+		return inventoryAnimation;
 	}
 
 }
