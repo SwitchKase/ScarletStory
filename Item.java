a1d15901128b4c13beab856a6259231354385551
Josh Wiedemeier <joshuaw-GitHub@jandjw.com>
2016-09-12 09:17:27 -0500

    adds get/set methods to item

--- Item.java
+++ Item.java
@@ -17,6 +17,33 @@ public class Item {
 		this.actions=actions;
 	}
 	
+	public String getType(){
+		return type;
+	}
+	public int getValue(){
+		return value;
+	}
+	public int getArmorValue(){
+		return armor;
+	}
+	public int getKeyID(){
+		return keyID;
+	}
+	
+	
+	public void setType(String t){
+		type=t;
+	}
+	public void setValue(int val){
+		value=val;
+	}
+	public void setArmor(int armorValue){
+		armor=armorValue;
+	}
+	public void setKeyID(int keyID){
+		this.keyID=keyID;
+	}
+	
 	public void doAction(int actionID){
 		actions[actionID].execute(this);
 	}
