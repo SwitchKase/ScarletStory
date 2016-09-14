5397cb32ea4cce567f2dbd28b98c133aff1ab05f
Josh Wiedemeier <joshuaw-GitHub@jandjw.com>
2016-09-14 09:01:43 -0500

    Fixes enemy constructors to match entity's

--- Enemy.java
+++ Enemy.java
@@ -6,12 +6,12 @@ public abstract class Enemy extends Entity{
     super();
   }
   
-  public Enemy(int currentHealth,int maxHealth,int currentMana,int maxMana,int speed,int crit,int posX,int posY, String type, Animation[] animations){
-    super(currentHealth,maxHealth,currentMana,maxMana,speed,crit,posX,posY,animations,type);
+  public Enemy(int currentHealth,int maxHealth,int currentMana,int maxMana,int armor,int speed,int posX,int posY,int vx,int vy,int width, int height, String type, Animation[] animations){
+    super(currentHealth,maxHealth,currentMana,maxMana,armor,speed,posX,posY,vx,vy,width,height,type,animations);
   }
   
-  public Enemy(int maxHealth,int maxMana,int speed,int crit,int posX,int posY, String type, Animation[] animations){
-    super(maxHealth,maxHealth,maxMana,maxMana,speed,crit,posX,posY,animations,type);
+  public Enemy(int maxHealth,int maxMana,int armor,int speed,int posX,int posY,int vx,int vy,int width, int height, String type, Animation[] animations){
+    super(maxHealth,maxHealth,maxMana,maxMana,armor,speed,posX,posY,vx,vy,width,height,type,animations);
   }
   
   public abstract void act(ArrayList<Entity> entityList);
